d6421b161b476e13dd13473735c10894bd828ce9
methodmenon <methodmenon@gmail.com>
2014-07-30 17:34:36 -0700

    Updated discount calculator project

--- discount_calculator.py
+++ discount_calculator.py
@@ -1,15 +1,34 @@
 def calculate_discount(item_cost, relative_discount, absolute_discount):
-	item_cost = item_cost * 1.0
-	relative_discount = relative_discount / 100.0
-	absolute_discount = absolute_discount
-
-	item_cost = item_cost * (1 - relative_discount)
-	item_cost = item_cost - absolute_discount
-
-	return item_cost
+	while (item_cost >= 0):
+		item_cost = item_cost * 1.0
+		relative_discount = relative_discount / 100.0
+		if (relative_discount > item_cost):
+			return"Relative discount is too high"
+		else:
+			absolute_discount = absolute_discount
+			item_cost = item_cost * (1 - relative_discount)
+			if (absolute_discount > item_cost):
+				return "Absolute discount is too high"
+			else:
+				item_cost = item_cost - absolute_discount
+				return item_cost
+	else:
+		return "Item cost cannot be a negative number"
 
 def main():
+	"""
+	Verifying that the function works
+	"""
 	item_cost = calculate_discount(100, 10, 30)
+	print item_cost
+
+	item_cost = calculate_discount(-100, 10, 30)
+	print item_cost
+
+	item_cost = calculate_discount(100, 20000, 30)
+	print item_cost
+
+	item_cost = calculate_discount(100, 10, 1000)
 	print item_cost
 
 if __name__ == "__main__":
--- test_discount_calculator.py
+++ test_discount_calculator.py
@@ -4,11 +4,26 @@ from discount_calculator import calculate_discount
 
 class DiscountCalculatorTests(unittest.TestCase):
 	def testAccurateDiscount(self):
-	#test 1 --> verify right values are returned for normal cases
-	discount_price = calculate_discount	(100, 10, 30)
+	#case 1 --> verify right values are returned for normal cases
+		discount_price = calculate_discount	(100, 10, 30)
 
-	self.assertEqual(discount_price, 60)
+		self.assertEqual(discount_price, 60)
 
-#case 1 --> final cost should never go below zero
+	#case 2 --> Item cost should never go below zero
+	def testItemCostNotNegative(self):
+		discount_price = calculate_discount(-100, 10, 30)
+		self.assertEqual(discount_price, "Item cost cannot be a negative number")
 
-#case 2 --> discounts can never be greater than 
+
+	#case 3 --> discounts can never be greater than item cost
+	def testRelativeDiscountTooHigh(self):
+		discount_price = calculate_discount(100, 20000, 30)
+		self.assertEqual(discount_price, "Relative discount is too high")
+
+	#case 4 --> absolute discount cannot be greater than item cost
+	def testAbsoluteDiscountTooHigh(self):
+		discount_price = calculate_discount(100, 10, 1000)
+		self.assertEqual(discount_price, "Absolute discount is too high")
+
+if __name__ == "__main__":
+	unittest.main()
